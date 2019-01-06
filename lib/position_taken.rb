ea7b52cf74afbb7d6e58ad38e213e7437a576546
Justin Davis <davisjustinw@gmail.com>
2019-01-06 00:10:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  board[index] != ' ' && board[index] != '' && !board[index]
+  if board[index] == ' '
+    return FALSE
+  else
+    return TRUE
+  end
 end
